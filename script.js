e2e9400da8b187993803f40902a255c88c8e98b8
peter <peter_cook@comcast.com>
2022-07-27 18:45:42 -0400

    assignment27 working conditionals

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function(){
-let form =document.querySelector("form");
+let form =document.getElementById("launchForm");
 form.addEventListener("submit", function(event){
+   event.preventDefault();
    let pilotName = document.querySelector("input[name=pilotName]");
    let copilotName =document.querySelector("input[name=copilotName]");
    let fuelLevel =document.querySelector("input[name=fuelLevel]");
    let cargoMass =document.querySelector("input[name=cargoMass]");
-   if (pilotName.value===""||copilotName.value===""){
-      alert("all fields are required");
-   }elseIf (fuelLevel.value(isNaN)||cargoMass.value(isNaN)){
-      alert("please enter a number")
-      event.preventDefault();
-      
-}
+   if (pilotName.value===""||copilotName.value==="" || fuelLevel.value===""||cargoMass.value===""){
+      alert("we arent going anywhere until you fill the forms in!!!");
+   }else if (isNaN(pilotName.value)===false||isNaN(copilotName)===false){
+      alert("Did your parents really name you a number")
+   }else if (isNaN(fuelLevel.value)||isNaN(cargoMass.value)){
+      alert("Thats not a number, wierdo")
+     
+   
+      //let launchInfo=[pilotName,copilotName,fuelLevel,pilotName];
+      }
 
 
 
@@ -27,7 +31,7 @@ form.addEventListener("submit", function(event){
 
 
 
-}
+});
 
 });
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
