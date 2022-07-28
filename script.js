779942203718afd0616acc86d7a056b0528119dc
peter <peter_cook@comcast.com>
2022-07-28 16:22:29 -0400

    added fuel and cargo conditionals

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ window.addEventListener("load", function(){
     let copilotStatus = document.getElementById("copilotStatus");
     let fuelStatus = document.getElementById("fuelStatus");
    let cargoMass = document.getElementById("cargoStatus");
+   let launchStatus = document.getElementById("launchStatus");
+   
    form.addEventListener("submit", function(event){
       event.preventDefault();
       let itemStatus = document.getElementById("itemStatus");
@@ -22,16 +24,32 @@ window.addEventListener("load", function(){
       alert("Thats not a number, wierdo")
    }else{
      itemStatus.style.visibility = "visible"
+     pilotStatus.innerHTML =`pilot: ${pilotInfo} is ready for launch`
+     copilotStatus.innerHTML = `copilot: ${copilotInfo} is ready for launch`
+     cargoMass.innerHTML =  `Cargo check passed`
+     fuelStatus.innerHTML = `Fuel level check passed`
+   };
+     if (fuelLevelInfo <=9999){
+        fuelStatus.innerHTML =`Insufficient Fuel`  
+        launchStatus.innerHTML = `Shuttle not ready for Launch`
+        launchStatus.style.color="red"
+     }if (cargoMassInfo>=10001){
+      cargoMass.innerHTML =  `Shuttle is to heavy, try a diet, or get rid of some stuff`
+      launchStatus.innerHTML=`Shuttle not ready for Launch`
+      launchStatus.style.color="red"
+   };
+
+
+
+
+      
+
+      
       
       
       
          
-         pilotStatus.innerHTML =pilotInfo
-         copilotStatus.innerHTML = copilotInfo
-         fuelStatus.innerHTML = fuelLevelInfo
-         cargoMass.innerHTML =  cargoMassInfo
       
-   }
 
 
 
